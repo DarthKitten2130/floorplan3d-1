c53b3ed031ef7d438d0c81bda98da615e5f8bda6
Sathvik Mulukutla <72032381+DarthKitten2130@users.noreply.github.com>
2022-08-31 00:29:17 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
-import blender
+#import blender
+from lib2to3.pytree import convert
 import os
 import kivy
 from kivy.app import App
@@ -6,7 +7,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivymd.uix.screen import Screen
 from kivy.uix.image import Image
-from kivy.uix.widget import Widget
+#from kivy.uix.widget import Widget
 
 
 
@@ -29,19 +30,28 @@ class Kivyapp(App):
         pos_hint={'center_x':0.5, 'center_y':0.8}, font_size='17sp',
         color=[0.6,1,0,1],  font_name='Cour')
 
+        
+
+        p1 = Image(source='dog.jpg')
+
         b1 = Button(text ='Convert to 3D', size=(1,1), size_hint=(.15,.08),
         pos =(430,50))
-        
+
+        #Lambda Functions
+        up = lambda x: screen.add_widget(p1) 
+        down = lambda y: screen.remove_widget(p1)
+
+        b1.bind(on_press = self.convert)
         #Image Opener
-        b2 = Button(text ='View Image', size=(1,1), size_hint=(.15,.08),
+        b2 = Button(text ='Image', size=(1,1), size_hint=(.15,.08),
         pos =(250,50))
-        up = lambda x: screen.add_widget(Image(source='dog.jfif')) 
         b2.bind(on_press = up)
+        
         try:
             os.mkdir("C:\\Users\\Student\\Desktop\\floorplan")
         except:
             pass
-
+   
         
 
         screen.add_widget(l1)
@@ -49,7 +59,14 @@ class Kivyapp(App):
         screen.add_widget(b1)
         screen.add_widget(b2)
         return screen
+    
+    def convert(*args):
+        l3 = Label(text='Converting Map to 3D Model; this may take a while...',
+                pos_hint={'center_x':0.5, 'center_y':0.2}, font_size='17sp',
+                color=[0.6,1,0,1],  font_name='Cour')
+        screen.add_widget(l3)
 
+Kivyapp().run()
         
 
 
