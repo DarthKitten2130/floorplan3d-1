ee95930e5247eba02370a3ed4a80591bd645b688
Sathvik Mulukutla <72032381+DarthKitten2130@users.noreply.github.com>
2022-09-01 00:08:22 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -49,9 +49,10 @@ class Kivyapp(App):
         b2 = Button(text ='Image', size=(1,1), size_hint=(.15,.08),
         pos =(250,50))
         b2.bind(on_press = up)
-        
+        save = "C:\\Users\\Student\\Desktop\\floorplan\\3D-Models"
         try:
             os.mkdir("C:\\Users\\Student\\Desktop\\floorplan")
+            os.mkdir(save)
         except:
             pass
    
@@ -68,6 +69,7 @@ class Kivyapp(App):
                 pos_hint={'center_x':0.5, 'center_y':0.2}, font_size='17sp',
                 color=[0.6,1,0,1],  font_name='Cour')
         screen.add_widget(l3)
-        sub.Popen(["blender","C:\\Users\\Hp\\Desktop\\floorplan3d\\ARCHI_2\\archi-20220830T050415Z-001\\archi\\saves\\2.blend"])
+        sub.Popen(["blender","map.blend"])
+
 
 Kivyapp().run()
