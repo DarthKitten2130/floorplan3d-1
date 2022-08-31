210facb4b8c878b71efc17f5b45cec96bf44925a
Sathvik Mulukutla <72032381+DarthKitten2130@users.noreply.github.com>
2022-08-31 12:55:01 +0530

    Delete blender.py

--- blender.py
+++
@@ -1,8 +0,0 @@
-import Image
-imp = Image.load(r"")
-if img,getname() == 2:
-  print("Image from :", img.getFilename())
-  print("loaded to obj : ", img.getname())
-  img.setxrep(4)
-  img.setyrep(2)
-  print(img.get())
